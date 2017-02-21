eaa811ac28588d63e98b260f2b0dafa611dd55e4
anishnath <anish2good@yahoo.co.in>
2017-02-20 22:10:11 -0500

    Fixed the Variables

--- openssl/aes_ige.cc
+++ openssl/aes_ige.cc
@@ -10,6 +10,7 @@
 #include <stddef.h>
 #include <stdint.h>
 #include <string>
+#include <openssl/rand.h>
 
 #define MAX_VECTOR_SIZE 64
 
@@ -22,21 +23,23 @@ bool LLVMFuzzerAES_ige(const uint8_t *Data, size_t Size) {
       
         unsigned char ckey[] =  "MIICXQIBAAKBgQCJMfzpf1Y3RHwMRfIFLCbWsCXaofCL11gi16Nfp1RBpGBZbnCGMIICXQIBAAKBgQCJMfzpf1Y3RHwMRfIFLCbWsCXaofCL11gi16Nfp1RBpGBZbnC1";
 	unsigned char ivec[] = "01234567890123456";
-	int bytes_read;
+        const size_t ivsize = AES_BLOCK_SIZE*2;
+        unsigned char iv_enc[ivsize], iv_dec[ivsize];
+        RAND_bytes(iv_enc, ivsize);
+        memcpy(iv_dec, iv_enc, ivsize);
 	unsigned char indata[MAX_VECTOR_SIZE];
-	unsigned char outdata[Size];
-	unsigned char decryptdata[Size];
+	unsigned char outdata[MAX_VECTOR_SIZE*2];
+	unsigned char decryptdata[MAX_VECTOR_SIZE*2];
 	/* data structure that contains the key itself */
 	AES_KEY keyEn;
 	/* set the encryption key */
 	AES_set_encrypt_key(Data, 128, &keyEn);
 	/* set where on the 128 bit encrypted block to begin encryption*/
-	int num = 0;
-	bytes_read = sizeof(indata);
+        const size_t encslength = ((128 + AES_BLOCK_SIZE) / AES_BLOCK_SIZE) * AES_BLOCK_SIZE; 
 
         //AES_ige_encrypt
-        AES_ige_encrypt (Data, outdata, bytes_read, &keyEn, ivec, AES_ENCRYPT);
-        AES_ige_encrypt (Data, outdata, bytes_read, &keyEn, ivec, AES_DECRYPT);
+        AES_ige_encrypt (Data, outdata, encslength, &keyEn, iv_dec, AES_ENCRYPT);
+        AES_ige_encrypt (Data, outdata, encslength, &keyEn, iv_dec, AES_DECRYPT);
        
       
         return 0;
